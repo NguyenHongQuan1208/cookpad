2d53c0fc78defa735318f0144105c14049f2ffc7
NguyenHongQuan <nquan1208@gmail.com>
2025-10-27 00:17:02 +0700

    add Bottom tab

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,37 +1,45 @@
-import 'package:cooking_pad/navigation/route_names.dart';
-import 'package:cooking_pad/network/services/auth_service.dart';
-import 'package:cooking_pad/utils/helpers/call_supabase_api.dart';
+import 'package:cooking_pad/screens/save/save_tab.dart';
+import 'package:cooking_pad/screens/search/search_tab.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _tabs = <Widget>[
+    const SearchTab(),
+    const SavedTab(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('HomeScreen'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final authService = AuthService();
-
-                await callSupabaseAuthApi<void>(
-                  service: () => authService.signOut(),
-                  context: context,
-                  onSuccess: (data) => context.go(Routes.signin),
-                  showSuccessToast: false,
-                  showErrorToast: false,
-                );
-              },
-              child: const Text('Logout'),
-            ),
-          ],
-        ),
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.search_outlined),
+            selectedIcon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bookmark_outline),
+            selectedIcon: Icon(Icons.bookmark),
+            label: 'Saved',
+          ),
+        ],
       ),
     );
   }
