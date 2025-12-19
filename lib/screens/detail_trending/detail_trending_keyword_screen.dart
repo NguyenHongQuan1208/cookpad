40df9332ea59a7dd4d77083d153915c2867ef1b7
NguyenHongQuan <nquan1208@gmail.com>
2025-12-19 14:37:52 +0700

    use flutter hooks lint

--- lib/screens/detail_trending/detail_trending_keyword_screen.dart
+++ lib/screens/detail_trending/detail_trending_keyword_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 
-class DetailKeywordsScreen extends HookWidget {
+class DetailKeywordsScreen extends StatelessWidget {
   const DetailKeywordsScreen({super.key});
 
   @override
